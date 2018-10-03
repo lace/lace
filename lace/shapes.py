4aba64b75b341a82ad0ea2aa54647382ec5299da
Paul Melnikow <github@paulmelnikow.com>
2018-10-03 18:50:26 -0400

    Delegate shape geometry generation to blmath (#15)

--- lace/shapes.py
+++ lace/shapes.py
@@ -1,9 +1,13 @@
-import math
-import numpy as np
-from lace.mesh import Mesh
+from blmath.geometry import shapes
 
 
-def create_rectangular_prism(origin, size):
+def _shape_as_mesh(shape_factory_fn, *args, **kwargs):
+    from lace.mesh import Mesh
+    vertices, faces = shape_factory_fn(*args, ret_unique_vertices_and_faces=True, **kwargs)
+    return Mesh(v=vertices, f=faces)
+
+
+def create_rectangular_prism(*args, **kwargs):
     '''
     Return a Mesh which is an axis-aligned rectangular prism. One vertex is
     `origin`; the diametrically opposite vertex is `origin + size`.
@@ -11,32 +15,10 @@ def create_rectangular_prism(origin, size):
     size: 3x1 array.
 
     '''
-    from lace.topology import quads_to_tris
-
-    lower_base_plane = np.array([
-        # Lower base plane
-        origin,
-        origin + np.array([size[0], 0, 0]),
-        origin + np.array([size[0], 0, size[2]]),
-        origin + np.array([0, 0, size[2]]),
-    ])
-    upper_base_plane = lower_base_plane + np.array([0, size[1], 0])
-
-    vertices = np.vstack([lower_base_plane, upper_base_plane])
-
-    faces = quads_to_tris(np.array([
-        [0, 1, 2, 3],  # lower base (-y)
-        [7, 6, 5, 4],  # upper base (+y)
-        [4, 5, 1, 0],  # +z face
-        [5, 6, 2, 1],  # +x face
-        [6, 7, 3, 2],  # -z face
-        [3, 7, 4, 0],  # -x face
-    ]))
-
-    return Mesh(v=vertices, f=faces)
+    return _shape_as_mesh(shapes.create_rectangular_prism, *args, **kwargs)
 
 
-def create_cube(origin, size):
+def create_cube(*args, **kwargs):
     '''
     Return a mesh with an axis-aligned cube. One vertex is `origin`; the
     diametrically opposite vertex is `size` units along +x, +y, and +z.
@@ -44,48 +26,30 @@ def create_cube(origin, size):
     size: int or float.
 
     '''
-    return create_rectangular_prism(origin, np.repeat(size, 3))
+    return _shape_as_mesh(shapes.create_cube, *args, **kwargs)
 
 
-def create_triangular_prism(p1, p2, p3, height):
+def create_triangular_prism(*args, **kwargs):
     '''
     Return a Mesh which is a triangular prism whose base is the triangle
     p1, p2, p3. If the vertices are oriented in a counterclockwise
     direction, the prism extends from behind them.
 
     '''
-    from blmath.geometry import Plane
-
-    base_plane = Plane.from_points(p1, p2, p3)
-    lower_base_to_upper_base = height * -base_plane.normal # pylint: disable=invalid-unary-operand-type
-    vertices = np.vstack(([p1, p2, p3], [p1, p2, p3] + lower_base_to_upper_base))
-
-    faces = np.array([
-        [0, 1, 2],  # base
-        [0, 3, 4], [0, 4, 1],  # side 0, 3, 4, 1
-        [1, 4, 5], [1, 5, 2],  # side 1, 4, 5, 2
-        [2, 5, 3], [2, 3, 0],  # side 2, 5, 3, 0
-        [5, 4, 3],  # base
-    ])
-
-    return Mesh(v=vertices, f=faces)
+    return _shape_as_mesh(shapes.create_triangular_prism, *args, **kwargs)
 
 
-def create_horizontal_plane():
+def create_horizontal_plane(*args, **kwargs):
     '''
     Creates a horizontal plane.
     '''
-    v = np.array([
-        [1., 0., 0.],
-        [-1., 0., 0.],
-        [0., 0., 1.],
-        [0., 0., -1.]
-    ])
-    f = [[0, 1, 2], [3, 1, 0]]
-    return Mesh(v=v, f=f)
+    return _shape_as_mesh(shapes.create_horizontal_plane, *args, **kwargs)
 
 
-if __name__ == '__main__':
+def _main():
+    import math
+    import numpy as np
+
     points = np.array([
         [1, 0, 0],
         [0, math.sqrt(1.25), 0],
@@ -96,3 +60,7 @@ if __name__ == '__main__':
 
     cube = create_cube(np.array([1., 0., 0.]), 4.)
     cube.show()
+
+
+if __name__ == '__main__':
+    _main()
